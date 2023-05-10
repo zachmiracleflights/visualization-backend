4b05d325f94e740f6417cd2bfee4b456ee1a7996
Zach McMullen <zachmcmullen04@gmail.com>
2023-05-09 20:34:39 -0700

    adding departures and returns

--- dist/index.mjs
+++ dist/index.mjs
@@ -24,11 +24,40 @@ const formatTime = (date) => {
     return timeString;
 }
 
-app.get("/flights", async (req, res) => {
+app.get("/departures", async (req, res) => {
 
     base('Flight Legs').select({
         maxRecords: 10,
-        view: "Today"
+        view: "Today's Departures"
+    }).firstPage((err, records) => {
+        if (err) {
+            console.log(err)
+            res.json(err)
+        }
+
+        const flightList = []
+
+        records?.forEach((record) => {
+            flightList.push(
+                {
+                    time: formatTime(new Date(record.get("Departure Date/Time"))),
+                    airline: record.get("Airline").substring(record.get("Airline").indexOf("-") + 1).trim(),
+                    to: record.get("State (from Arrival Airport)"),
+                    from: record.get("State (from Departure Airport)"),
+                    flight: record.get("Flight Number")
+                }
+            )
+        })
+
+        res.json(flightList)
+    })
+})
+
+app.get("/returns", async (req, res) => {
+
+    base('Flight Legs').select({
+        maxRecords: 10,
+        view: "Today's Returns"
     }).firstPage((err, records) => {
         if (err) {
             console.log(err)
