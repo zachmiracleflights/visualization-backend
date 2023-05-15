d21f34efde41d1ac73cbe295cff682591f44889e
Zach McMullen <zachmcmullen04@gmail.com>
2023-05-15 12:12:20 -0700

    [Update] No longer formatting time in backend

--- dist/index.mjs
+++ dist/index.mjs
@@ -13,17 +13,6 @@ Airtable.configure({
 })
 const base = Airtable.base("appQcfrcSc0lfgpQH")
 
-const formatTime = (date) => {
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    let timeString = hours + ':' + minutes + ' ' + ampm;
-    return timeString;
-}
-
 app.get("/departures", async (req, res) => {
 
     base('Flight Legs').select({
@@ -40,12 +29,12 @@ app.get("/departures", async (req, res) => {
         records?.forEach((record) => {
             flightList.push(
                 {
-                    time: formatTime(new Date(record.get("Departure Date/Time"))),
+                    time: new Date(record.get("Departure Date/Time")),
                     airline: record.get("Airline").substring(record.get("Airline").indexOf("-") + 1).trim(),
                     to: record.get("State (from Arrival Airport)"),
                     from: record.get("State (from Departure Airport)"),
                     flight: record.get("Flight Number"),
-                    arrival_time: formatTime(new Date(record.get("Arrival Date/Time")))
+                    arrival_time: new Date(record.get("Arrival Date/Time"))
                 }
             )
         })
@@ -70,12 +59,12 @@ app.get("/returns", async (req, res) => {
         records?.forEach((record) => {
             flightList.push(
                 {
-                    time: formatTime(new Date(record.get("Departure Date/Time"))),
+                    time: new Date(record.get("Departure Date/Time")),
                     airline: record.get("Airline").substring(record.get("Airline").indexOf("-") + 1).trim(),
                     to: record.get("State (from Arrival Airport)"),
                     from: record.get("State (from Departure Airport)"),
                     flight: record.get("Flight Number"),
-                    arrival_time: formatTime(new Date(record.get("Arrival Date/Time")))
+                    arrival_time: new Date(record.get("Arrival Date/Time"))
                 }
             )
         })
